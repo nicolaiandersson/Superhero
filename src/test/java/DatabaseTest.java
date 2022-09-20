a359bf8b335942a9e21b2670b5436feb542eb5f8
nicolaiandersson <nico602d@stud.kea.dk>
2022-09-20 23:40:58 +0200

    added DatabaseTest to do junit tests

---
+++ src/test/java/DatabaseTest.java
@@ -0,0 +1,27 @@
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+    Database database;
+
+    @BeforeEach
+    public void beforeTest() {
+        database = new Database();
+    }
+
+
+    @org.junit.jupiter.api.Test
+    void createOneHero() {
+        //Arrange
+        database.createSuperhero("Hulk", "Stærk", "Bruce Banner",1970,true);
+        int expected = 1;
+        //Act
+        int actual = database.getSuperheroes().size();
+        //Assert
+        assertEquals(expected,actual);
+    }
+    
+
+}
